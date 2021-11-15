432165155319f55f4fa383a3742dc1b9dc24ab38
TRETYAKweb <tretyakbro@gmail.com>
2021-11-15 04:55:00 +0300

    added section reviews

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,29 @@
 $(function () {
 
+  $('.reviews__list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    dots: true,
+  });
+
 });
 
-
-$('.menu-btn').on('click', function(){
+$('.menu-btn').on('click', function () {
   $('.menu').addClass('active');
 });
 
-$('.clouse-btn').on('click', function(){
+$('.clouse-btn').on('click', function () {
   $('.menu').removeClass('active');
 });
+
+// new Swiper({
+//   el: '#reviews-slider',
+//   slidesPerView: 3,
+// });
+
+
 // $('.menu__btn, .menu a').on('click', function () {
 //   $('.menu__list, .menu__btn').toggleClass('menu__list--active');
 //   $('body').toggleClass('open-menu');
