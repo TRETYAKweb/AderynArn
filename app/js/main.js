7c86ddc9008af5d70ecba871f9c379c56cf87cb4
TRETYAKweb <tretyakbro@gmail.com>
2021-11-15 06:55:09 +0300

    added section my-works

--- app/js/main.js
+++ app/js/main.js
@@ -18,13 +18,3 @@ $('.clouse-btn').on('click', function () {
   $('.menu').removeClass('active');
 });
 
-// new Swiper({
-//   el: '#reviews-slider',
-//   slidesPerView: 3,
-// });
-
-
-// $('.menu__btn, .menu a').on('click', function () {
-//   $('.menu__list, .menu__btn').toggleClass('menu__list--active');
-//   $('body').toggleClass('open-menu');
-// });
